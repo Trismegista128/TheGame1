fab3ca1e1d21a1f4aba381579e0e3c47b8946018
PKUB <pkub@oticonmedical.com>
2020-05-14 23:03:15 +0200

    Added scripts to sorl objects within game are so they render in front of player if player is behind them and in back of a player if player is in front of them

---
+++ TheGame/Assets/Scripts/Helpers/RenderingOrderOnLoad.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class RenderingOrderOnLoad : MonoBehaviour
+{
+    public float ObjectHeight;
+
+    private SpriteRenderer renderer;
+    void Start()
+    {
+        var component = GetComponent<SpriteRenderer>();
+        renderer = component ?? GetComponentInChildren<SpriteRenderer>();
+
+        renderer.sortingOrder = (int)((renderer.transform.position.y + ObjectHeight) * -100);
+    }
+}
---
+++ TheGame/Assets/Scripts/Helpers/RenderingOrderSystem.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class RenderingOrderSystem : MonoBehaviour
+{
+    private SpriteRenderer renderer;
+    void Start()
+    {
+        var component = GetComponent<SpriteRenderer>();
+        renderer = component ?? GetComponentInChildren<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        renderer.sortingOrder = (int)(renderer.transform.position.y * -100);
+    }
+}
