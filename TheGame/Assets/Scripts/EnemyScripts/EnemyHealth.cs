81324a1147d8d4e22134761e600f15c221ad382a
PKUB <pkub@oticonmedical.com>
2020-05-18 22:32:17 +0200

    Enemy once again can be killed. Removed unwanted scripts

---
+++ TheGame/Assets/Scripts/EnemyScripts/EnemyHealth.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public float HP;
+    public GameManager manager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var isBullet = collision.GetComponent<Bullet>();
+        if(isBullet != null)
+        {
+            //Destroy bullet
+            //Warning, this may be already too late and bullet could hit already another target.
+            Destroy(collision.transform.gameObject);
+
+            //figure out what damage.
+            LoseLife(1f);
+        }
+    }
+
+    private void LoseLife(float damage)
+    {
+        if(HP > damage)
+        {
+            HP -= damage;
+        }
+        else
+        {
+            manager.EnemyKilled();
+            Destroy(this.gameObject);
+        }
+    }
+}
